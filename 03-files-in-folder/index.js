856d23be7054a240d391bf8cc25eabe0f04d8013
Maria Kagermanova <maria.kgm04@gmail.com>
2022-05-26 10:36:46 +0300

    fix weight of files

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,10 @@ const path = require('path');
 
 const PATH = path.join(__dirname, 'secret-folder');
 
-let allFiles;
-
 fs.readdir(PATH, {withFileTypes: true}, (err, files) => {
   if (err) console.error(err.message);
-  allFiles = files;
-  allFiles.forEach(file =>
-    fs.stat(path.join(PATH, 'text.txt'), (err, file_stat) => {
+  files.forEach(file =>
+    fs.stat(path.join(PATH, file.name), (err, file_stat) => {
       if (err) console.error(err.message);
       else if (file.isFile()) {
         console.log(path.basename(file.name, path.extname(file.name)), '-' , path.extname(file.name).substring(1, path.extname(file.name).length), '-', file_stat.size * 0.001 + 'kb');
@@ -17,3 +14,4 @@ fs.readdir(PATH, {withFileTypes: true}, (err, files) => {
     })
   );
 })
+ 
